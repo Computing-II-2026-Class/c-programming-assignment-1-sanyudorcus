9814fbcfa8cd1c1969d3b1f6539bc0102414f826
sanyudorcus <sanyudorcus2025@gmail.com>
2026-03-24 20:13:53 +0300

    Update ex2_fuel.c

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -3,9 +3,20 @@
 
 #include <stdio.h>
 
-int main(void)
-{
-    
+int main(void){
+
+float distance_travelled;
+float fuel_used;
+float fuel_efficiency;
+
+print("Enter distance covered (km)");
+scanf("%f", &distance_travelled);
+
+print("Enter fuel used (litres)");
+scanf("%f", &fuel_used);
+
+fuel_efficiency = (distance_travelled/fuel_used);
+printf("Fuel efficiency:%.2fkm/l",fuel_efficiency);
 
     return 0;
 }
