7bc60fe0e85d66159be06515e4a68cf5421846c9
sanyudorcus <sanyudorcus2025@gmail.com>
2026-03-24 18:58:28 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,11 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+//Name: Sanyu Dorcus 
+// Student Number: 25/U/BIE/01420/PE
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+  
 
     return 0;
 }
