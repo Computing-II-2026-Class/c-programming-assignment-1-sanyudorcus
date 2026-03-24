da83ce5a05276532661fc17e414aac8ccba32958
sanyudorcus <sanyudorcus2025@gmail.com>
2026-03-24 19:53:11 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -3,9 +3,22 @@
 
 #include <stdio.h>
 
-int main(void)
-{
-    
+int main(void){
+float amount;
+float trans;
+float amount_deducted;
+
+printf("Enter amount to send");
+scanf("%f", & amount);
+
+printf("Enter transaction fee");
+scanf("%f", &trans );
+
+transaction = amount * (trans/100.00);
+amount_deducted = (amount + transaction);
+
+printf("Transaction fee:2f\n", transaction);
+printf("Amount deducted:%2f", amount_deducted);
 
     return 0;
 }
